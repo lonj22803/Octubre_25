b69051655261c24e9d9a8f530a7e09e87ec42886
lonj22803 <j.londono123@utp.edu.co>
2025-10-05 21:09:30 +0200

    Errores con apturl corregidos

---
+++ week_one/montaje_RAG/a_prueba.py
@@ -0,0 +1,7 @@
+"""
+Dado que el experimento fue resuleto facilmenete por modelo como GPT-5, Gemini, Calude y Mistral, se hizo una prueba
+con una implementacion local, usando Mistral 7B, los resultados no fueron para nada buenos, un modelo que no tiene la
+capacidad de razonamiento sobre busqueda sobre un directorio que al final, es un grafo, no puede resolver este tipo de
+problemas
+
+"""
